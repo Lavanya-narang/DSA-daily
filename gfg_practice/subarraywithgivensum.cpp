74a5a78e365eea2e2a42264c8015bd06caf61f1f
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2022-08-03 10:57:52 +0530

    Update subarraywithgivensum.cpp

--- gfg_practice/subarraywithgivensum.cpp
+++ gfg_practice/subarraywithgivensum.cpp
@@ -1,40 +1,24 @@
-#include <iostream>
-using namespace std;
-
-/* Returns true if the there is a subarray of 
-arr[] with a sum equal to 'sum' otherwise 
-returns false. Also, prints the result */
-int subArraySum(int arr[], int n, int sum)
-{
-    /* Initialize curr_sum as value of 
-    first element and starting point as 0 */
-    int curr_sum = arr[0], start = 0, i;
-
-    /* Add elements one by one to curr_sum and 
-    if the curr_sum exceeds the sum,
-    then remove starting element */
-    for (i = 1; i <= n; i++) {
-        // If curr_sum exceeds the sum,
-        // then remove the starting elements
-        while (curr_sum > sum && start < i - 1) {
-            curr_sum = curr_sum - arr[start];
-            start++;
+        vector<int> ans;
+        int cursum=arr[0];
+        int start=0;
+        for(int i=1;i<=n;i++)
+        {
+            while(cursum>s && start<i-1)
+            {
+                cursum=cursum-arr[start];
+                start++;
+            }
+            if(cursum==s)
+            {
+                ans.push_back(start+1); //position = index + 1
+                ans.push_back(i-1+1); 
+                return ans;
+            }
+            if(i<n)
+            {
+                cursum=cursum+arr[i];
+            }
+            
         }
-
-        // If curr_sum becomes equal to sum,
-        // then return true
-        if (curr_sum == sum) {
-            cout << "Sum found between indexes "
-                 << start << " and " << i - 1;
-            return 1;
-        }
-
-        // Add this element to curr_sum
-        if (i < n)
-            curr_sum = curr_sum + arr[i];
-    }
-
-    // If we reach here, then no subarray
-    cout << "No subarray found";
-    return 0;
-}
+        ans.push_back(-1);
+        return ans;
