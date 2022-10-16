153323124ba0dbe7e950c6f25ef4fa43c7a5eaf6
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2022-10-16 12:24:20 +0530

    Create get_common_element_2.cpp

---
+++ Level 1/Hashmap Heaps/get_common_element_2.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  //write your code here
+    int n,m;
+    cin>>n;
+    int arr1[n];
+    unordered_map<int,int>mp;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr1[i];
+        mp[arr1[i]]++;
+    }
+    cin>>m;
+    int arr2[m];
+    for(int i=0;i<m;i++)
+    {
+        cin>>arr2[i];
+        if(mp.find(arr2[i])!=mp.end() && mp[arr2[i]]!=0)
+        {
+            cout<<arr2[i]<<endl;
+            mp[arr2[i]]--;
+        }
+    }
+}
