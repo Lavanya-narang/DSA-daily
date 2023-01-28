8e1c111b8bcb103c371ca4448845d91eeb9ec3c6
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-01-28 17:24:16 +0530

    Create no+of_ways_coins_can_get_sum.cpp

---
+++ dpyt/unbounded/no+of_ways_coins_can_get_sum.cpp
@@ -0,0 +1,27 @@
+int ways(int coins[],int sum,int n)
+{
+    int dp[n+1][sum+1];
+    for(int i=0;i<n+1;i++)
+    {
+        for(int j=0;j<sum+1;j++)
+        {
+            if(i==0)
+            {
+                dp[i][j]=0;
+            }
+            if(j==0)
+            {
+                dp[i][j]=1;
+            }
+            if(coins[i-1]<=j)
+            {
+                dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
+            }
+            else
+            {
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+    return dp[n][sum];
+}
