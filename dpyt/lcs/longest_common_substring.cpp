6e6b91b2a71046d4bfada2d0756f0905ba73446f
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-01-29 14:26:23 +0530

    Create longest_common_substring.cpp

---
+++ dpyt/lcs/longest_common_substring.cpp
@@ -0,0 +1,27 @@
+int lcsubstr(string x,string y,int n,int m,int dp[n][m])  
+{  
+    for(int i=0;i<m+1;i++)
+    {
+        for(int j=0;j<n+1;j++)
+        {
+            if(i==0 || j==0)
+            {
+            dp[i][j]=0;
+            }
+        }
+    }
+    for(int i=1;i<m+1;i++)
+    {
+        for(int j=1;j<n+1;j++)
+        {
+            if(x[i-1]==y[j-1])
+            {
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=0;
+            }
+        }
+    }
+    return dp[n][m];
+}
