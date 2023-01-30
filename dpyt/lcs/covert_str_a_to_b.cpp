1971477d7d1a959e4e64150833daabb5770ffabe
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-01-30 18:57:32 +0530

    Create covert_str_a_to_b.cpp

---
+++ dpyt/lcs/covert_str_a_to_b.cpp
@@ -0,0 +1,26 @@
+//min no of insertion deletion
+int convertatob(string a,string b,int m,int n)
+{
+    int dp[m+1][n+1];
+    for(int i=0;i<m+1;i++)
+    {
+        for(int j=0;j<n+1;j++)
+        {
+            if(i==0 || j==0)
+            {
+                dp[i][j]=0;
+            }
+            else if(a[i-1]==b[j-1])
+            {
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else
+            {
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    int insert,dlt;
+    insert=m-dp[m][n];
+    dlt=n-dp[m][n];
+}
