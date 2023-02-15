12b1ccbad0a05a80658a0b8881a1add8a6a834f4
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-02-15 12:29:41 +0530

    Update 26_remove_duplicate_from_sorted_array.cpp

--- Leetcode/top_interview_questions/26_remove_duplicate_from_sorted_array.cpp
+++ Leetcode/top_interview_questions/26_remove_duplicate_from_sorted_array.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        vector<int>ans;
-        vector<int>:: iterator i;
-        for(i=nums.begin();i!=nums.end();i++)
-        {
-            int ele=*i;
-            if(find(ans.begin(),ans.end(),ele)!=ans.end())
-            {
-                nums.erase(i);
-                i--;
-            }
-            else
-            {
-                ans.push_back(*i);
-            }
-        }
-        return nums.size();
+       int index=1;
+       for(int i=1;i<nums.size();i++)
+       {
+           if(nums[i-1]!=nums[i])
+           {
+               nums[index]=nums[i];
+               index++;
+           }
+       }
+       return index;
     }
 };
