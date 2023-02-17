0af6ce787c08ee75011fc62268c848eb136668c7
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-02-17 11:34:23 +0530

    Create 783_min_distance_btw_bst_nodes.cpp

---
+++ Leetcode/daily_challenge/783_min_distance_btw_bst_nodes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    TreeNode* prev=NULL;
+    void solve(TreeNode* root,int& ans)
+    {
+        if(root->left)
+        {
+            solve(root->left,ans);
+        }
+        if(prev!=NULL)
+        {
+            ans=min(ans,abs(prev->val-root->val));
+        }
+        prev=root;
+        if(root->right)
+        {
+            solve(root->right,ans);
+        }
+    }
+    int minDiffInBST(TreeNode* root) {
+        int ans=INT_MAX;
+        solve(root,ans);
+        return ans;
+    }
+};
