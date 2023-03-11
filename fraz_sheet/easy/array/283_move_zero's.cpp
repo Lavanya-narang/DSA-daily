3a1c6c99a6164abfbb067e9eb758708526e769c8
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-03-11 18:31:06 +0530

    Create 283_move_zero's.cpp

---
+++ fraz_sheet/easy/array/283_move_zero's.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) 
+    {
+        int n=nums.size();
+        if(n==0 || n==1)
+        {
+            return;
+        }
+        int left=0,right=0,temp;
+        while(right<n)
+        {
+            if(nums[right]==0)
+            {
+                right++;
+            }
+            else
+            {
+                swap(nums[left],nums[right]);
+                left++;
+                right++;
+            }
+        }
+    }
+};
