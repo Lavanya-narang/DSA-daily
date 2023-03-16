26032fb0c2acb9f239d3301b95d38341a87f9b1c
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-03-16 12:07:20 +0530

    Create 509_fibonacci.cpp

---
+++ fraz_sheet/easy/array/509_fibonacci.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fib(int n) 
+    {
+        if(n<=1)
+        {
+            return n;
+        }
+        // return fib(n-1)+fib(n-2);
+        int a=0,b=1,c,count=2;
+        while(count<=n)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+            count++;
+        }
+        return c;
+    }
+};
