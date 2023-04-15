5c0a861adb937122874f6c536cd4cf4bf2a5ddc5
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-04-15 05:38:26 +0530

    Create 1221_split_in_balanced_string.cpp

---
+++ Leetcode/practice/strings/easy/1221_split_in_balanced_string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) 
+    {
+        //TC:O(n) SC:O(1)
+        int ans=0,total=0;
+        for(auto i:s)
+        {
+            if(i=='L')
+            {
+                total++;
+            }
+            else
+            {
+                total--;
+            }
+            if(total==0)
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+    
+};
+Footer
