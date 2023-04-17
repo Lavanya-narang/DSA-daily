e728074e73de25cc867746c414dc2fdf67dc9fe1
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-04-17 05:36:55 +0530

    Create 2500_delete_greatest_value_in_row.cpp

---
+++ Leetcode/practice/matrix/easy/2500_delete_greatest_value_in_row.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int deleteGreatestValue(vector<vector<int>>& grid) 
+    {
+        int n=grid.size(),m=grid[0].size();
+        int ans=0;
+       for(int i=0;i<n;i++)
+       {
+           sort(grid[i].begin(),grid[i].end());
+       }
+       for(int i=0;i<m;i++)
+       {
+           int mn=INT_MIN;
+           for(int j=0;j<n;j++)
+           {
+               mn=max(mn,grid[j][i]);
+           }
+           ans+=mn;
+       }
+        return ans;
+
+    }
+};
