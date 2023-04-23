bcf51eb58d1563f1c46be8edbdaac028df67bb54
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-04-23 07:07:15 +0530

    Create 2433_find_original_array_of_prefix_XOR.cpp

---
+++ Leetcode/practice/array/medium/2433_find_original_array_of_prefix_XOR.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) 
+    {
+        int n=pref.size();
+        vector<int>ans(n);
+        
+        if(n==1)
+        {
+            return pref;
+        }
+        ans[0]=pref[0];
+        for(int i=1;i<n;i++)
+        {
+            ans[i]=pref[i-1]^pref[i];
+            
+        }
+        return ans;
+    }
+};
