57d092186088522ebfd2223c4ef695f1ad5446fd
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-05-09 07:53:28 +0530

    Update 54_spiral_matrix.cpp

--- Leetcode/daily_challenge/54_spiral_matrix.cpp
+++ Leetcode/daily_challenge/54_spiral_matrix.cpp
@@ -44,3 +44,42 @@
 //         return ans;
 //     }
 // };
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) 
+    {
+        int m=matrix.size(),n=matrix[0].size();
+        int sr=0,sc=0,ec=n-1,er=m-1;
+        vector<int>ans;
+        while(sr<=er and sc<=ec)
+        {
+            for(int j=sc;j<=ec;j++)
+            {
+                ans.push_back(matrix[sr][j]);
+            }
+            sr++;
+            for(int i=sr;i<=er;i++)
+            {
+                ans.push_back(matrix[i][ec]);
+            }
+            ec--;
+            if(sr<=er)
+            {
+            for(int j=ec;j>=sc;j--)
+            {
+                ans.push_back(matrix[er][j]);
+            }
+            er--;
+            }
+            if(sc<=ec)
+            {
+            for(int i=er;i>=sr;i--)
+            {
+                ans.push_back(matrix[i][sc]);
+            }
+            sc++;
+            }
+        }
+        return ans;
+    }
+};
