a4077599344ee5f623afec777bcdc6545513516d
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-05-12 19:12:23 +0530

    Create 643_max_avg_salary.cpp

---
+++ Leetcode/practice/sliding window/easy/643_max_avg_salary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k)
+    {
+        int i=0,j=0,n=nums.size();
+        int mx=INT_MIN;
+        int ans=0;
+        while(j<nums.size())
+        {
+            ans+=nums[j];
+            if(j-i+1<k)
+            {
+                j++;
+            }
+            else if(j-i+1==k)
+            {
+                mx=max(mx,ans);
+                ans-=nums[i];
+                i++;j++;
+            }
+            
+        }
+        return mx/(double)k;
+    }
+};
