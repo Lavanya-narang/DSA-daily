cc744ecde0ea300c31966d31a39beb7ce335f134
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-05-14 06:54:11 +0530

    Update 2089_find_target_indies.cpp

--- Leetcode/practice/binary search/easy/2089_find_target_indies.cpp
+++ Leetcode/practice/binary search/easy/2089_find_target_indies.cpp
@@ -16,3 +16,39 @@ public:
         return ans;
     }
 };
+
+
+
+// class Solution {
+// public:
+// vector<int>ans;
+// void bs(vector<int>& nums, int target,int low,int high)
+// {
+//     if(low>high)
+//     {
+//         return;
+//     }
+//     int mid=low+(high-low)/2;
+//     if(nums[mid]==target)
+//     {
+//         bs(nums,target,low,mid-1);
+//         ans.push_back(mid);
+//         bs(nums,target,mid+1,high);
+//     }
+//     else if(nums[mid]>target)
+//     {
+//         bs(nums,target,low,mid-1);
+//     }
+//     else
+//     {
+//         bs(nums,target,mid+1,high);;
+//     }
+// }
+//     vector<int> targetIndices(vector<int>& nums, int target) 
+//     {
+//         sort(nums.begin(),nums.end());
+//         int low=0,high=nums.size()-1;
+//        bs(nums,target,0,nums.size()-1);  
+//         return ans;
+//     }
+// };
