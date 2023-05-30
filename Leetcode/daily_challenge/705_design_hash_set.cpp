1a50e99d6dd7c42f0a1bf210c81f1fdf619c58f6
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-05-30 07:32:27 +0530

    Create 705_design_hash_set.cpp

---
+++ Leetcode/daily_challenge/705_design_hash_set.cpp
@@ -0,0 +1,41 @@
+class MyHashSet {
+     int numBuckets;
+    vector<list<int>> buckets;
+    
+    int hash_function(int key){
+        return key % numBuckets;
+    }
+public:
+    MyHashSet() {
+        numBuckets = 15000;
+        buckets = vector<list<int>>(numBuckets, list<int>{});
+    }
+    
+    void add(int key) {
+        int i = hash_function(key);
+        if(find(buckets[i].begin(), buckets[i].end(), key) == buckets[i].end())
+            buckets[i].push_back(key);
+    }
+    
+    void remove(int key) {
+        int i = hash_function(key);
+        auto it = find(buckets[i].begin(), buckets[i].end(), key);
+        if(it != buckets[i].end())
+            buckets[i].erase(it);
+    }
+    
+    bool contains(int key) {
+         int i = hash_function(key);
+        if(find(buckets[i].begin(), buckets[i].end(), key) == buckets[i].end())
+            return false;
+        return true;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
