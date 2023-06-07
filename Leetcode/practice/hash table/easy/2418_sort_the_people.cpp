23f0ed1b6286d47d04a31b710015c2e2cc987401
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-06-07 06:42:56 +0530

    Create 2418_sort_the_people.cpp

---
+++ Leetcode/practice/hash table/easy/2418_sort_the_people.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) 
+    {
+        vector<string>ans;
+        map<int,string>mp;
+        for(int i=0;i<names.size();i++)
+        {
+            mp[heights[i]]=names[i];
+        }
+        for(auto it=mp.rbegin();it!=mp.rend();it++)
+        {
+            ans.push_back(it->second);
+        }
+        return ans;
+    }
+};
