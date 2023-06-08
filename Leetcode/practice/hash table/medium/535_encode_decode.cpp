11ae59118d4aed65ae2bd9d181dbea79a62ebda1
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-06-08 07:55:42 +0530

    Create 535_encode_decode.cpp

---
+++ Leetcode/practice/hash table/medium/535_encode_decode.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    //map<string,string>mp;
+    //int num=0;
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) 
+    {
+        // num++;
+        // string addon=to_string(num);
+        // string ans="http://tinyurl.com/";
+        // ans+=string(addon);
+        // mp[ans]=longUrl;
+        // return ans;   
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        //return mp[shortUrl];
+        return shortUrl;
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
