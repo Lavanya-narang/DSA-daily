c88461f997371d19368cdf80374b41bf28927f82
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-08-25 07:36:14 +0530

    Create 443. String Compression.cpp

---
+++ Leetcode/Leetcode 75/Medium/Array&String/443. String Compression.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int compress(vector<char>& chars)
+    {
+        chars.push_back('~');
+        int n=chars.size();
+        vector<char>ans;
+        int total=1;
+        for(int i=1;i<n;i++)
+        {
+            if(chars[i]==chars[i-1])
+            {
+                total++;
+            }
+            else
+            {
+                if(total==1)
+                {
+                    ans.push_back(chars[i-1]);
+                    total=1;
+                }
+                else
+                {
+                    ans.push_back(chars[i-1]);
+                    string totlen=to_string(total);
+                    for(auto i:totlen)
+                    {
+                        ans.push_back((char)i);
+                    }
+                    total=1;
+                }
+            }
+        }
+        chars=ans;
+        return ans.size();
+    }
+};
