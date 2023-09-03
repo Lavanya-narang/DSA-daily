12b4f4efcd925da006189ec14f03cacaaa733893
Lavanya Narang <82329482+Lavanya-narang@users.noreply.github.com>
2023-09-03 10:43:16 +0530

    Create 1732. Find the Highest Altitude.cpp

---
+++ Leetcode/Leetcode 75/Easy/Prefix Sum/1732. Find the Highest Altitude.cpp
@@ -0,0 +1,62 @@
+// class Solution {
+// public:
+//     int largestAltitude(vector<int>& gain) 
+//     {
+//         int mx=0,curr=0;
+//        for(int i=0;i<gain.size();i++)
+//        {
+//            curr+=gain[i];
+//            mx=max(curr,mx);
+//        }
+//        return mx;
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     int largestAltitude(vector<int>& gain) 
+//     {
+//         vector<int> altitudes;
+//         int sum=0;
+//         for(int i=0; i<gain.size(); i++)
+//         {
+//           altitudes.push_back(sum);
+//           sum=sum+gain[i];
+//         }
+//         altitudes.push_back(sum);
+//         return *max_element(altitudes.begin(),altitudes.end());
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     int largestAltitude(vector<int>& gain) {
+//         int ans =0, tmp=0;
+
+//         for (int i = 0; i < gain.size(); i++)
+//         {
+//           if(i!=0){
+//             gain[i] += gain[i-1];
+//           }
+//           ans = max(ans, gain[i]);
+//         }
+//         return ans;
+//     }
+// };
+
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int max=0,sum=0;
+        for(int i:gain)
+        {
+            sum+=i;
+            if(max<sum)
+            max=sum;
+        }
+        return max;
+    }
+};
